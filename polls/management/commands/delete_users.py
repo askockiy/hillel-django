7c71328fb5c36cd352925a1db1ae46a945dbe5de
Alexander.Skockiy <alexander.skockiy@scrdairy.com>
2023-01-21 10:52:38 +0200

    added delete_users

---
+++ polls/management/commands/delete_users.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help_text = 'Delete users by id'
+
+    def add_arguments(self, parser):
+        parser.add_argument('user_ids', nargs='+', type=int, help='List of user ids to delete')
+
+    def handle(self, *args, **kwargs):
+        user_ids = kwargs['user_ids']
+        su = User.objects.filter(is_superuser=True, id__in=user_ids)
+        if su.exists():
+            self.stdout.write(self.style.ERROR('Cannot delete superusers'))
+            return
+        User.objects.filter(id__in=user_ids).exclude(is_superuser=True).delete()
+        self.stdout.write(self.style.SUCCESS('Successfully deleted users with ids: {}'.format(user_ids)))
