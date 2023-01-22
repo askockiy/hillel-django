e26ce46b7511d939d11eb0f19172f4eab0ef3e03
Alexander.Skockiy <alexander.skockiy@scrdairy.com>
2023-01-22 10:19:53 +0200

    refactor if is_superuser

--- polls/management/commands/delete_users.py
+++ polls/management/commands/delete_users.py
@@ -6,13 +6,17 @@ class Command(BaseCommand):
     help_text = 'Delete users by id'
 
     def add_arguments(self, parser):
-        parser.add_argument('user_ids', nargs='+', type=int, help='List of user ids to delete')
+        parser.add_argument(
+            'user_ids',
+            nargs='+',
+            type=int,
+            help='List of user ids to delete'
+        )
 
     def handle(self, *args, **kwargs):
         user_ids = kwargs['user_ids']
-        su = User.objects.filter(is_superuser=True, id__in=user_ids)
-        if su.exists():
+        if User.objects.filter(is_superuser=True, id__in=user_ids).exists():
             self.stdout.write(self.style.ERROR('Cannot delete superusers'))
             return
-        User.objects.filter(id__in=user_ids).exclude(is_superuser=True).delete()
+        User.objects.filter(id__in=user_ids).delete()
         self.stdout.write(self.style.SUCCESS('Successfully deleted users with ids: {}'.format(user_ids)))
